4537e9c17d393199d03762ae797cd1b94cffef83
JengYoung <young_pallete@naver.com>
2022-09-24 19:56:00 +0900

    refactor: new.target 방어코드 및 테스트 코드 추가, callback 함수를 정상적으로 받을 수 있도록 조정
    다만 아쉬운 점이 있는데, new.target에 대한 테스트 코드는 결국 인스턴스 타입이 객체라, 이를 깊게 비교하는 로직이 필요합니다.
    이에 대해서는 코드가 더 많이 들 것 같아서, 간단하게 value를 같은 것을 호출하는지로 테스트했습니다!

--- JavaScript/6주차/assignment/재영/index.js
+++ JavaScript/6주차/assignment/재영/index.js
@@ -1,4 +1,8 @@
 const Caculator = function (val = 0) {  
+  if (!new.target) {
+    return new Caculator(val);
+  }
+
   this.initialize = (val) => {
     this.val = isNaN(Number(val)) ? 0 : val;
     
@@ -38,11 +42,9 @@ const Caculator = function (val = 0) {
   this.initialize(val);
 }
 
-const caculator = new Caculator(2);
+const caculator = Caculator(2);
 
-// console.log(caculator.add(5).multiply(3).divide(5).run());
-
-function expect(string = "", cb, options) {
+function expect(string = "", cb) {
   if (!new.target) {
     return new expect(string, cb);
   }
@@ -60,8 +62,9 @@ function expect(string = "", cb, options) {
   });
 
   this.toBeEqual = (val) => {
-    const res =  cb.run();
+    const res =  cb();
     const result = this.isNot ? res !== val : res === val;
+
     const nowIsNot = this.isNot;
 
     this.isNot = false;
@@ -70,8 +73,9 @@ function expect(string = "", cb, options) {
   }
 }
 
-expect("이 값은 4.2가 나와야 한다.", caculator.add(5).multiply(3).divide(5)).toBeEqual(4.2);
-expect("나누기를 0으로 하면 무한이 나온다.", caculator.divide(0)).toBeEqual(Infinity);
-expect("초기화를 하면 0이 나온다.", caculator.initialize()).toBeEqual(0);
-expect("음수더라도 정상적으로 값이 출력된다.", caculator.substract(5).divide(2)).toBeEqual(-2.5);
-expect("문자열이 주어진다면 일반적인 연산의 기댓값이 나오지 않는다.", caculator.add("1")).not.toBeEqual(-2.51);
+expect("new 함수를 호출하지 않더라도, 정상적인 인스턴스가 나와야 한다.", () => Caculator(123).val).toBeEqual(new Caculator(123).val)
+expect("이 값은 4.2가 나와야 한다.", () => caculator.add(5).multiply(3).divide(5).run()).toBeEqual(4.2);
+expect("나누기를 0으로 하면 무한이 나온다.", () => caculator.divide(0).run()).toBeEqual(Infinity);
+expect("초기화를 하면 0이 나온다.", () => caculator.initialize().run()).toBeEqual(0);
+expect("음수더라도 정상적으로 값이 출력된다.", () => caculator.substract(5).divide(2).run()).toBeEqual(-2.5);
+expect("문자열이 주어진다면 일반적인 연산의 기댓값이 나오지 않는다.", () => caculator.add("1").run()).not.toBeEqual(-2.51);
