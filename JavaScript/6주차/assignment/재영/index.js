75477b3334b747ef9a5c2c44e0ace9f8abebc31e
JengYoung <young_pallete@naver.com>
2022-09-24 19:46:59 +0900

    test: 테스트 코드 작성
    이번에 처음으로 한 번 테스트 라이브러리를 가상으로 구현해봤는데, 이런 느낌으로 실제로 동작할 수 있지 않을까 싶어요!
    특히 접근자 프로퍼티가 이런 식으로 쓰여진다는 건 몸소 체험하고 놀랐답니다.
    정말 재밌는 경험이네요 🥰

--- JavaScript/6주차/assignment/재영/index.js
+++ JavaScript/6주차/assignment/재영/index.js
@@ -1,6 +1,8 @@
 const Caculator = function (val = 0) {  
   this.initialize = (val) => {
     this.val = isNaN(Number(val)) ? 0 : val;
+    
+    return this;
   }
 
   this.add = (val) => {
@@ -28,7 +30,9 @@ const Caculator = function (val = 0) {
   }
 
   this.run = () => {
-    return this.val;
+    const result = this.val;
+
+    return result;
   }
 
   this.initialize(val);
@@ -36,4 +40,38 @@ const Caculator = function (val = 0) {
 
 const caculator = new Caculator(2);
 
-console.log(caculator.add(5).multiply(3).divide(5).run());
+// console.log(caculator.add(5).multiply(3).divide(5).run());
+
+function expect(string = "", cb, options) {
+  if (!new.target) {
+    return new expect(string, cb);
+  }
+
+  this.isNot = false;
+
+  Object.defineProperty(this, 'not', {
+    get() {
+      this.isNot = !this.isNot;
+      return this;
+    },
+  
+    set() {
+    }
+  });
+
+  this.toBeEqual = (val) => {
+    const res =  cb.run();
+    const result = this.isNot ? res !== val : res === val;
+    const nowIsNot = this.isNot;
+
+    this.isNot = false;
+    
+    console.log({ command: string, result, ...(!result ? { expectedValue: `${nowIsNot ? 'not ' : ''}${val}`, actualValue: res } : {}) });
+  }
+}
+
+expect("이 값은 4.2가 나와야 한다.", caculator.add(5).multiply(3).divide(5)).toBeEqual(4.2);
+expect("나누기를 0으로 하면 무한이 나온다.", caculator.divide(0)).toBeEqual(Infinity);
+expect("초기화를 하면 0이 나온다.", caculator.initialize()).toBeEqual(0);
+expect("음수더라도 정상적으로 값이 출력된다.", caculator.substract(5).divide(2)).toBeEqual(-2.5);
+expect("문자열이 주어진다면 일반적인 연산의 기댓값이 나오지 않는다.", caculator.add("1")).not.toBeEqual(-2.51);
