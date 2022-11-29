4c86ee1023add03876eb308f199adcc315960b7b
JengYoung <young_pallete@naver.com>
2022-11-29 23:59:31 +0900

    :recycle: expect를 그룹화하여 검사하는 describe 함수 추가

--- JavaScript/07주차/assignment/재영/test.js
+++ JavaScript/07주차/assignment/재영/test.js
@@ -27,49 +27,51 @@ function expect(string = "", cb) {
   }
 }
 
-try {
-  const tests = [
-    expect("3개를 추가한 후 현재를 반환하면, 길이가 3개가 나와야 한다.", () => {
-      const doubleLinkedList = new DoubleLinkedList();
-      return doubleLinkedList.insert(1).insert(2).insert(3).length;
-    }).toBeEqual(3),
-    expect("마지막을 삭제하면, 마지막은 첫번째 머리 노드가 나와야 한다.", () => {
-      const doubleLinkedList = new DoubleLinkedList();
-      return doubleLinkedList.insert(1).insert(2).insert(3).remove().nowNode.val;
-    }).toBeEqual(1),
-    expect("첫번째 머리 노드를 삭제하면, 추출 시 다음 노드의 앞은 꼬리 노드가 나와야 한다.", () => {
-      const doubleLinkedList = new DoubleLinkedList();
-      return doubleLinkedList.insert(1).insert(2).insert(3).goPrev().goPrev().remove().nowNode.val;
-    }).toBeEqual(2),
-    expect("첫번째 머리 노드를 삭제하면, 다음 머리는 다음 노드가 되어야 한다.", () => {
-      const doubleLinkedList = new DoubleLinkedList();
-      return doubleLinkedList.insert(1).insert(2).insert(3).goPrev().goPrev().remove().head.val;
-    }).toBeEqual(2),
-    expect("getNthNodeFromHead 메서드로 순회를 한다면 머리 노드를 기준으로 나와야 한다", () => {
-      const doubleLinkedList = new DoubleLinkedList();
-      return doubleLinkedList
-        .insert(1)
-        .insert(2)
-        .insert(3)
-        .insert(4)
-        .insert(5)
-        .insert(6)
-        .insert(7)
-        .insert(8)
-        .insert(9)
-        .getNthNode(8)
-        .val
-    }).toBeEqual(8)
-  ];
-
-  const res = tests.filter(v => v).length;
-  if (res === tests.length) {
-    console.log(`✅ CHECKED ${tests.length} TESTS SUCCEED!`)
-  } else {
-    throw new Error(`🔥 ${res}/${tests.length} TESTS SUCCEED. FAILED 😖`)
+function describe(title, tests) {
+  try {
+    console.log(`${title} 결과: `)
+    const res = tests.filter(v => v).length;
+    if (res === tests.length) {
+      console.log(`✅ CHECKED ${tests.length} TESTS SUCCEED!`)
+    } else {
+      throw new Error(`🔥 ${res}/${tests.length} TESTS SUCCEED. FAILED 😖`)
+    }
+  } catch(e) {
+    console.error("🤯 FAILED TEST!\n")
+    console.error(e)
   }
-
-} catch(e) {
-  console.error("🤯 FAILED TEST!\n")
-  console.error(e)
 }
+
+describe("양방향 연결리스트", [
+  expect("3개를 추가한 후 현재를 반환하면, 길이가 3개가 나와야 한다.", () => {
+    const doubleLinkedList = new DoubleLinkedList();
+    return doubleLinkedList.insert(1).insert(2).insert(3).length;
+  }).toBeEqual(3),
+  expect("마지막을 삭제하면, 마지막은 첫번째 머리 노드가 나와야 한다.", () => {
+    const doubleLinkedList = new DoubleLinkedList();
+    return doubleLinkedList.insert(1).insert(2).insert(3).remove().nowNode.val;
+  }).toBeEqual(1),
+  expect("첫번째 머리 노드를 삭제하면, 추출 시 다음 노드의 앞은 꼬리 노드가 나와야 한다.", () => {
+    const doubleLinkedList = new DoubleLinkedList();
+    return doubleLinkedList.insert(1).insert(2).insert(3).goPrev().goPrev().remove().nowNode.val;
+  }).toBeEqual(2),
+  expect("첫번째 머리 노드를 삭제하면, 다음 머리는 다음 노드가 되어야 한다.", () => {
+    const doubleLinkedList = new DoubleLinkedList();
+    return doubleLinkedList.insert(1).insert(2).insert(3).goPrev().goPrev().remove().head.val;
+  }).toBeEqual(2),
+  expect("getNthNodeFromHead 메서드로 순회를 한다면 머리 노드를 기준으로 나와야 한다", () => {
+    const doubleLinkedList = new DoubleLinkedList();
+    return doubleLinkedList
+      .insert(1)
+      .insert(2)
+      .insert(3)
+      .insert(4)
+      .insert(5)
+      .insert(6)
+      .insert(7)
+      .insert(8)
+      .insert(9)
+      .getNthNode(8)
+      .val
+  }).toBeEqual(8)
+]);
