6606795a38865b6e4c9744cba618df55d23c70e5
Minhye Kang <alsgp9898@naver.com>
2022-12-02 14:11:40 +0900

    feat: 기능 구현 완료

---
+++ JavaScript/07주차/assignment/7주차/민혜/index.js
@@ -0,0 +1,91 @@
+// 양방향 연결리스트는 각 노드의 prev와 next로 이동할 수 있어야 한다.
+class Node {
+  constructor(value) {
+    this.prev = null;
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  /*
+  push를 통해 양방향 연결리스트 꼬리에 값을 추가할 수 있다.
+  */
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  /*
+   insert를 통해 특정 인덱스의 노드에 삽입할 수 있어야 한다.
+   */
+  insert(index, value) {
+    if (index < 0 || index > index.length) return false;
+    if (index === this.length - 1) return !!this.push(value);
+
+    let newNode = new Node(value);
+    let previousNode = this.get(index);
+
+    newNode.next = previousNode.next;
+    newNode.prev = previousNode;
+    previousNode.next = newNode;
+    this.length++;
+
+    return true;
+  }
+
+  /*
+    get을 통해 원하는 인덱스의 노드를 가져온다.
+    */
+  get(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter < index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  /*
+   remove를 통해 해당 인덱스의 노드를 삭제할 수 있다.
+   */
+  remove(index) {
+    let currentNode = this.get(index);
+    let previousNode = currentNode.prev;
+    let afterNode = currentNode.next;
+
+    previousNode.next = afterNode;
+    afterNode.prev = previousNode;
+    this.length--;
+
+    return currentNode;
+  }
+
+  /*
+   update를 통해 해당 인덱스의 노드를 수정할 수 있다.
+   */
+  update(index, value) {
+    if (index < 0 || index > this.length) return false;
+
+    let currentNode = this.get(index);
+    currentNode.value = value;
+
+    return true;
+  }
+}
+
+const list = new DoublyLinkedList();
